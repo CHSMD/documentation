c0e8d4540007c8b9fe109ee8713e2630e06c303a
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-12-27 17:57:45 -0800

    added plant router

--- src/routes/plantRoutes.js
+++ src/routes/plantRoutes.js
@@ -1,21 +1,12 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
 const axios = require('axios');
 const plantRouter = express.Router();
-const bearerAuth = require('../auth/middleware/bearer.js');
 const permissions = require('../auth/middleware/acl');
+const { Router } = require('express');
 
-plantRouter.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false },
-}));
-
-plantRouter.get('/collection', bearerAuth, permissions('user'), async (req, res, next) => {
+plantRouter.get('/collection', async (req, res, next) => {
   try {
     const apiEndpoint = 'https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/collection';
 
@@ -26,7 +17,7 @@ plantRouter.get('/collection', bearerAuth, permissions('user'), async (req, res,
   }
 });
 
-plantRouter.get('/collection/:id',  bearerAuth, permissions('user'), async (req, res, next) => {
+plantRouter.get('/collection/:id', async (req, res, next) => {
   const apiEndpoint = `https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/collection/${req.params.id}`;
   try {
     const response = await axios.get(apiEndpoint);
@@ -36,7 +27,8 @@ plantRouter.get('/collection/:id',  bearerAuth, permissions('user'), async (req,
   }
 });
 
-plantRouter.put('/collection/:id', bearerAuth, permissions('admin'), async (req, res, next) => {
+
+plantRouter.put('/collection/:id', async (req, res, next) => {
   const apiEndpoint = `https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/collection/${req.params.id}`;
   try {
     const response = await axios.put(apiEndpoint, req.body);
@@ -46,7 +38,7 @@ plantRouter.put('/collection/:id', bearerAuth, permissions('admin'), async (req,
   }
 });
 
-plantRouter.delete('/collection/:id',  bearerAuth, permissions('admin'), async (req, res, next) => {
+plantRouter.delete('/collection/:id', async (req, res, next) => {
   const apiEndpoint = `https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/collection/${req.params.id}`;
   try {
     const response = await axios.delete(apiEndpoint);
@@ -56,14 +48,26 @@ plantRouter.delete('/collection/:id',  bearerAuth, permissions('admin'), async (
   }
 });
 
-plantRouter.post('/collection',  bearerAuth, permissions('admin'), async (req, res, next) => {
+plantRouter.post('/collection', async (req, res, next) => {
   const apiEndpoint = 'https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/collection';
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://house-plants.p.rapidapi.com/common/coralberry',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'e2b412e95bmshd819cfe55d2ac1bp14e845jsn7156cb08f7f5',
+  //     'X-RapidAPI-Host': 'house-plants.p.rapidapi.com',
+  //   },
+  // };
   try {
     const response = await axios.post(apiEndpoint, req.body);
     res.status(200).send(response.data);
   } catch (error) {
     next(error);
   }
+
 });
 
+
 module.exports = plantRouter;
+
+
