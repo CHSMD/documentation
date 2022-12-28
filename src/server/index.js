50dcac19400d380846059d8a17384b9af7a7403c
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-12-27 17:57:45 -0800

    added plant router

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const { Server } = require('socket.io');
 const { createServer } = require('http');
 const cors = require('cors');
+const authRoutes = require('../auth/routes/routes');
+const plantRoutes = require('../routes/plantRoutes');
 
 const app = express();
 const httpServer = createServer(app);
@@ -17,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(authRoutes);
+app.use(plantRoutes);
 
 module.exports = {
   server: io,
