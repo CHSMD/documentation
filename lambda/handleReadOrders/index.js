ce5187be5aaa5b7be1d6fc106cf3cb0fe1f193e3
Camilla Rees <camilla.c.rees@gmail.com>
2022-12-29 12:43:02 -0700

    added readOneOrder and readOrders lambda functions complete

---
+++ lambda/handleReadOrders/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const dynamoose = require('dynamoose');
+
+const orderSchema = new dynamoose.Schema({
+  orderNumber: Number,
+  plants: {
+    type: Array,
+    schema: [{
+      type: Object,
+      schema: {
+        id: Number,
+        quantity: Number,
+      },
+    }],
+  },
+  total: Number,
+  status: String,
+}, {
+  saveUnknown: true,
+});
+
+const Order = dynamoose.model('order-table', orderSchema);
+
+exports.handler = async (event) => {
+  try {
+    const orders = await Order.scan().exec();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(orders),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: 'No orders found' }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+};
