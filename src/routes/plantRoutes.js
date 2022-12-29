9b0c493b3f20cbe8dfbf629e62433e6c5a95d92b
dustinapodaca <dustinapodaca@gmail.com>
2022-12-28 19:40:24 -0700

    working placeOrder function and routes to add items to a cart

--- src/routes/plantRoutes.js
+++ src/routes/plantRoutes.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const express = require('express');
+const session = require('express-session');
 const axios = require('axios');
 const plantRouter = express.Router();
 const bearerAuth = require('../auth/middleware/bearer.js');
 const permissions = require('../auth/middleware/acl');
+
+plantRouter.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+}));
+
 
 plantRouter.get('/collection', bearerAuth, permissions('read'), async (req, res, next) => {
   try {
@@ -64,10 +73,58 @@ plantRouter.post('/collection',  bearerAuth, permissions('create'), async (req, 
   } catch (error) {
     next(error);
   }
+});
 
+// plantRouter.post('/add-to-order', bearerAuth, permissions('create'), async (req, res, next) => {
+//   const plantId = req.body.id;
+//   const quantity = req.body.quantity;
+
+//   req.session.order = req.session.order || [];
+//   req.session.order.push({ id: plantId, quantity });
+//   res.send({
+//     message: 'Plant added to order',
+//     order: req.session.order,
+//   });
+// });
+
+// plantRouter.post('/place-order', bearerAuth, permissions('create'), async (req, res, next) => {
+//   const apiEndpoint = 'https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/place-order';
+//   try {
+//     const orderData = { plants: req.session.order };
+//     const response = await axios.post(apiEndpoint, orderData);
+
+//     req.session.order = [];
+//     res.send(response.data);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+plantRouter.post('/add-to-order', async (req, res, next) => {
+  const plantId = req.body.id;
+  const quantity = req.body.quantity;
+
+  req.session.order = req.session.order || [];
+  req.session.order.push({ id: plantId, quantity });
+  res.send({
+    message: 'Plant added to order',
+    order: req.session.order,
+  });
+});
+
+plantRouter.post('/place-order', async (req, res, next) => {
+  const apiEndpoint = 'https://cognb1larg.execute-api.us-west-2.amazonaws.com/plantspace/place-order';
+  try {
+    const orderData = { plants: req.session.order };
+    console.log(orderData);
+    const response = await axios.post(apiEndpoint, orderData);
+
+    req.session.order = [];
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 
 module.exports = plantRouter;
-
-
