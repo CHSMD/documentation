577694b2d0208735eed7f6bde0b146f7a4740bc9
dustinapodaca <dustinapodaca@gmail.com>
2022-12-29 16:03:39 -0700

    cleaned code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 require('dotenv').config();
-
 const { db } = require('./src/auth/models');
 const ioExpress = require('./src/server/index');
 const PORT = process.env.PORT || 3002;
--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -4,7 +4,6 @@ const { users } = require('../models/index');
 
 module.exports = async (req, res, next) => {
   try {
-
     if (!req.headers.authorization) { _authError();}
 
     const token = req.headers.authorization.split(' ')[1];
