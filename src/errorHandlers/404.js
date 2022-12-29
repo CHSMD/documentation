7ca753631880a02d23eba15455e2309d8b785671
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-12-29 10:47:47 -0800

    added 404 and 500 error handler

--- src/errorHandlers/404.js
+++ src/errorHandlers/404.js
@@ -1,9 +1,10 @@
 'use strict';
 
+
 module.exports = (req, res, next) => {
   res.status(404).send({
     error: 404,
     route: req.baseUrl,
-    message: 'ERROR: Route Not Found',
+    message: 'Not found',
   });
 };
